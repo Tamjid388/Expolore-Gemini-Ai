b73b4c67b98f6a893b6e1b801d0018abe92d7298
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2025-02-01 13:40:08 +0600

    Added /test-ai route for AI response

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express=require("express")
 const app=express();
+require("dotenv").config()
 const port=process.env.PORT || 5000;
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
+
+
+
+
+
+
+app.get('/test-ai',async(req,res)=>{
+    const prompt = "difference between pixel and apple";
+
+    const result = await model.generateContent(prompt);
+    console.log(result.response.text());
+    res.send({answer: result.response.text()})
+})
 
 
 app.get('/',(req,res)=>{
